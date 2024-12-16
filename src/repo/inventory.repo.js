8f45ec061ebeb770627f640bf7dd611c7bc20feb
Hnam <hnam4527@gmail.com>
2024-12-17 00:19:41 +0700

    update

--- src/repo/inventory.repo.js
+++ src/repo/inventory.repo.js
@@ -1,22 +1,50 @@
 'use strict'
 
 const { inventory } = require("../models/inventory.model")
+const { convertToObjectIdMongodb } = require("../utils")
 
 
-const insertInventory = async({
-    productId, 
-    shopId, 
-    stock, 
+const insertInventory = async ({
+    productId,
+    shopId,
+    stock,
     location = 'unknown'
 }) => {
     return await inventory.create({
-        productId, 
-        shopId, 
-        stock, 
+        productId,
+        shopId,
+        stock,
         location
     })
 }
 
+const reservationInventory = async ({ productId, quantity, cartId }) => {
+    const query = {
+        productId: convertToObjectIdMongodb(productId),
+        stock: { $gte: quantity },
+    }
+    const updateSet = {
+        $inc: {
+            stock: -quantity,
+        },
+        $push: {
+            reservation: {
+                quantity,
+                cartId,
+                createOn: new Date()
+            }
+        }
+    }
+
+    const options = {
+        upsert: true,
+        new: true
+    }
+
+    return await inventory.findOneAndUpdate(query, updateSet, options)
+}
+
 module.exports = {
-    insertInventory
+    insertInventory, 
+    reservationInventory
 }
--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -1,23 +1,47 @@
-'use strict'
+"use strict";
 
-const redis = require('redis')
-const { promisify } = require('util')
-const redisClient = redis.createClient()
+const redis = require("redis");
+const { promisify } = require("util");
+const { reservationInventory } = require("../repo/inventory.repo");
+const redisClient = redis.createClient();
 
-const pExpire = promisify(redisClient.pExpire).bind(redisClient)
-const setNXAsync = promisify(redisClient.setNX).bind(redisClient)
+const pExpire = promisify(redisClient.pExpire).bind(redisClient);
+const setNXAsync = promisify(redisClient.setNX).bind(redisClient);
 
 const acquireLock = async ({ productId, quantity, cartId }) => {
-    const key = `lock_v2024_${productId}`
-    const retryTimes = 10
-    const expireTime = 3_000
+    const key = `lock_v2024_${productId}`;
+    const retryTimes = 10;
+    const expireTime = 3_000;
 
-    for (let i = 0; i < retryTimes; i++){
+    for (let i = 0; i < retryTimes; i++) {
         // create key:  who can pay
-        const result = await setNXAsync(key, expireTime)
+        const result = await setNXAsync(key, expireTime);
 
-        console.log(result)
+        console.log(result);
 
-        // if ()
+        if (result === 1) {
+            const isReservation = await reservationInventory({
+                productId,
+                cartId,
+                quantity
+            })
+            if (isReservation.modifiedCount) {
+                await pExpire(key, expireTime)
+                return key
+            }
+            return null;
+        } else {
+            await new Promise((resolve) => setTimeout(resolve, 50));
+        }
     }
+};
+
+const releaseLock = async (keyLock) => {
+    const delAsyncKey = promisify(redisClient.del).bind(redisClient)
+    return await delAsyncKey(keyLock)
+}
+
+module.exports = {
+    acquireLock,
+    releaseLock
 }
